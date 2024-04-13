abd3459aafad4802506ae46147c919a5d036ae0b
Felipe Manchester <f.p2809@gmail.com>
2024-04-13 11:11:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Helper/ProtectedRoute';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='login/*' element={<Login />} />
+            <Route
+              path='conta/*'
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Helper/Error.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Error = ({ error }) => {
+  if (!error) return null;
+  return <p className='error'>{error}</p>;
+};
+export default Error;
