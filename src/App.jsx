6eb73a27d4ccfacf6337cc4a86f2738b76a3588d
Felipe Manchester <f.p2809@gmail.com>
2024-06-16 08:28:08 -0300

    lost password

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
@@ -25,6 +28,9 @@ function App() {
                 </ProtectedRoute>
               }
             />
+            <Route path='foto/:id' element={<Photo />} />
+            <Route path='perfil/:user' element={<UserProfile />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
