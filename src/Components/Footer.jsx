ed21008b19e2495cd6eaecf46260144fa03b9df9
Felipe Manchester <f.p2809@gmail.com>
2024-06-18 13:51:47 -0300

    Create Chart with Victory lib

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import Dogs from '../Assets/dogs-footer.svg?react';
 
 const Footer = () => {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+      <p>
+        Projeto criado pela{' '}
+        <a href='https://origamid.com' target='_blank'>
+          Origamid
+        </a>
+        . Desenvolvido por{' '}
+        <a href='https://linkedin.com/in/felipemanchester' target='_blank'>
+          Felipe Manchester
+        </a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
